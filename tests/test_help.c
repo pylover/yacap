4d0169e457beb27b01416106f35fd34497a74f9b
pylover <vahid.mardani@gmail.com>
2024-07-12 01:03:03 +0330

    Feature: --help/-h and --usage/-?

--- tests/test_help.c
+++ tests/test_help.c
@@ -82,6 +82,7 @@ test_help_doc() {
 
 void
 test_help_nooptions() {
+    char *help;
     struct carg carg = {
         .args = "FOO",
         .header = NULL,
@@ -91,16 +92,29 @@ test_help_nooptions() {
         .flags = CARG_NO_HELP
     };
 
-    eqint(CARG_ERR, carg_parse_string(&carg, "foo --help", NULL));
+    eqint(CARG_ERROR, carg_parse_string(&carg, "foo --help", NULL));
     eqstr("", out);
-    eqstr("foo: unrecognized option '--help'\n"
+    eqstr("foo: invalid option -- '--help'\n"
         "Try `foo --help' or `foo --usage' for more information.\n", err);
 
     carg.flags = CARG_NO_CLOG | CARG_NO_USAGE;
-    char *help =
+    help =
         "Usage: foo [OPTION...] FOO\n"
         "\n"
-        "  -h, --help    Give this help list\n"
+        "  -h, --help    Give this help list and exit\n"
+        "\n"
+        "Lorem ipsum footer\n";
+
+    eqint(CARG_OK_EXIT, carg_parse_string(&carg, "foo --help", NULL));
+    eqstr(help, out);
+    eqstr("", err);
+
+    carg.flags = CARG_NO_CLOG;
+    help =
+        "Usage: foo [OPTION...] FOO\n"
+        "\n"
+        "  -h, --help     Give this help list and exit\n"
+        "  -?, --usage    Give a short usage message and exit\n"
         "\n"
         "Lorem ipsum footer\n";
 
@@ -137,50 +151,48 @@ test_help_default() {
 }
 
 
-// void
-// test_help_options() {
-//     struct carg_option options[] = {
-//         {"foo", 'f', NULL, 0, "Foo flag"},
-//         {"bar", 'b', "BAR", 0, "Bar option with value"},
-//         {"baz", 'z', "BAZ", 0, LOREM},
-//         {"qux", 1, "QUX",  0, NULL},
-//         {NULL}
-//     };
-//
-//     struct carg carg = {
-//         .args = NULL,
-//         .header = NULL,
-//         .options = options,
-//         .footer = "Lorem ipsum footer",
-//         .version = NULL,
-//     };
-//
-//     char *help =
-// "Usage: foo [OPTION...]\n"
-// "\n"
-// "  -f, --foo                Foo flag\n"  // NOLINT
-// "  -b, --bar=BAR            Bar option with value\n"  // NOLINT
-// "  -z, --baz=BAZ            Lorem merol ipsum dolor sit amet, consectetur adipi-\n"  // NOLINT
-// "                           scing elit, sed do eiusmod tempor incididunt ut lab-\n"  // NOLINT
-// "                           ore et dolore magna aliqua. Ut enim ad minim veniam,\n"  // NOLINT
-// "                           quis nostrud exercitation ullamco laboris nisi ut a-\n"  // NOLINT
-// "                           liquip ex ea commodo consequat. Duis aute irure dol-\n"  // NOLINT
-// "                           or.\n"  // NOLINT
-// "      --qux=QUX            \n"  // NOLINT
-// "  -h, --help               Give this help list\n"  // NOLINT
-// "  -?, --usage              Give a short usage message\n"  // NOLINT
-// "  -v, --verbose[=LEVEL]    Verbosity level. one of: '0|s|silent', '1|f|fatal', \n"  // NOLINT
-// "                           '2|e|error', '3|w|warn', '4|i|info' and '5|d|debug'.\n"  // NOLINT
-// "                           if this option is not given, the verbosity level wi-\n"  // NOLINT
-// "                           ll be '3|w|warn', but If option is given without va-\n"  // NOLINT
-// "                           lue, then the verbosity level will be '4|i|info'.\n"  // NOLINT
-// "\n"
-// "Lorem ipsum footer\n";
-//
-//     eqint(CARG_OK_EXIT, carg_parse_string(&carg, "foo --help", NULL, NULL));
-//     eqstr(help, out);
-//     eqstr("", err);
-// }
+void
+test_help_options() {
+    struct carg_option options[] = {
+        {"foo", 'f', NULL, 0, "Foo flag"},
+        {"bar", 'b', "BAR", 0, "Bar option with value"},
+        {"baz", 'z', "BAZ", 0, LOREM},
+        {"qux", 1, "QUX",  0, NULL},
+        {NULL}
+    };
+
+    struct carg carg = {
+        .args = NULL,
+        .header = NULL,
+        .options = options,
+        .footer = "Lorem ipsum footer",
+        .version = NULL,
+    };
+
+    char *help =
+"Usage: foo [OPTION...]\n"
+"\n"
+"  -f, --foo              Foo flag\n"
+"  -b, --bar=BAR          Bar option with value\n"
+"  -z, --baz=BAZ          Lorem merol ipsum dolor sit amet, consectetur adipisc-\n"
+"                         ing elit, sed do eiusmod tempor incididunt ut labore\n"
+"                         et dolore magna aliqua. Ut enim ad minim veniam, quis\n"
+"                         nostrud exercitation ullamco laboris nisi ut aliquip\n"
+"                         ex ea commodo consequat. Duis aute irure dolor.\n"
+"      --qux=QUX          \n"
+"  -h, --help             Give this help list and exit\n"
+"  -?, --usage            Give a short usage message and exit\n"
+"  -v, --verbose=LEVEL    Verbosity level. one of: '0|s|silent', '1|f|fatal', '2\n"
+"                         |e|error', '3|w|warn', '4|i|info' and '5|d|debug'. if\n"
+"                         this option is not given, the verbosity level will be\n"
+"                         '3|w|warn'\n"
+"\n"
+"Lorem ipsum footer\n";
+
+    eqint(CARG_OK_EXIT, carg_parse_string(&carg, "foo --help", NULL));
+    eqstr(help, out);
+    eqstr("", err);
+}
 
 
 int
@@ -188,7 +200,7 @@ main() {
     test_usage();
     test_help_doc();
     test_help_default();
-    // test_help_nooptions();
-    // test_help_options();
+    test_help_nooptions();
+    test_help_options();
     return EXIT_SUCCESS;
 }
