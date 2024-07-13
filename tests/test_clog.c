e3b043098ccab30b30887c2c54371d3c51643ddc
pylover <vahid.mardani@gmail.com>
2024-07-13 10:29:39 +0330

    Implementing clog integrity

--- tests/test_clog.c
+++ tests/test_clog.c
@@ -40,101 +40,53 @@ test_verbose_short() {
     eqint(CARG_OK, carg_parse_string(&carg, "foo", NULL));
     eqint(CLOG_WARNING, clog_verbosity);
 
-    // clog_verbosity = -1;
-    // eqint(CARG_OK, carg_parse_string(&carg, "foo -v", NULL));
-    // eqint(CLOG_SILENT, clog_verbosity);
+    eqint(CARG_OK, carg_parse_string(&carg, "foo -v", NULL));
+    eqint(CLOG_INFO, clog_verbosity);
 
-    // clog_verbosity = -1;
-    // eqint(CARG_OK, carg_parse_string(&carg, "foo -vv", NULL));
-    // eqint(CLOG_DEBUG, clog_verbosity);
+    eqint(CARG_OK, carg_parse_string(&carg, "foo -vv", NULL));
+    eqint(CLOG_DEBUG, clog_verbosity);
 
-    // clog_verbosity = -1;
-    // eqint(CARG_OK, carg_parse_string(&carg, "foo -v2", NULL));
-    // eqint(CLOG_ERROR, clog_verbosity);
-
-    // clog_verbosity = -1;
-    // eqint(CARG_OK, carg_parse_string(&carg, "foo -v0", NULL));
-    // eqint(CLOG_SILENT, clog_verbosity);
-
-    // clog_verbosity = -1;
-    // eqint(CARG_OK, carg_parse_string(&carg, "foo -ve", NULL));
-    // eqint(CLOG_ERROR, clog_verbosity);
-
-    // clog_verbosity = -1;
-    // eqint(CARG_OK, carg_parse_string(&carg, "foo -vw", NULL));
-    // eqint(CLOG_WARNING, clog_verbosity);
-
-    // clog_verbosity = -1;
-    // eqint(CARG_OK, carg_parse_string(&carg, "foo -vi", NULL));
-    // eqint(CLOG_INFO, clog_verbosity);
-
-    // clog_verbosity = -1;
-    // eqint(CARG_OK, carg_parse_string(&carg, "foo -vd", NULL));
-    // eqint(CLOG_DEBUG, clog_verbosity);
-
-    // clog_verbosity = -1;
-    // eqint(CARG_ERROR, carg_parse_string(&carg, "foo -vx", NULL));
-    // eqstr("", out);
-    // eqstr("foo: '-vx' option, invalid argument: x\n"
-    //     "Try `foo --help' or `foo --usage' for more information.\n", err);
-
-    // clog_verbosity = -1;
-    // eqint(CARG_ERROR, carg_parse_string(&carg, "foo -v6", NULL));
-    // eqstr("", out);
-    // eqstr("foo: '-v6' option, invalid argument: 6\n"
-    //     "Try `foo --help' or `foo --usage' for more information.\n", err);
-
-    // clog_verbosity = -1;
-    // eqint(CARG_ERROR, carg_parse_string(&carg, "foo -v-1", NULL));
-    // eqstr("", out);
-    // eqstr("foo: '-v-1' option, invalid argument: -1\n"
-    //     "Try `foo --help' or `foo --usage' for more information.\n", err);
-
-    // clog_verbosity = -1;
-    // eqint(CARG_ERROR, carg_parse_string(&carg, "foo -vvv", NULL));
-    // eqstr("", out);
-    // eqstr("foo: '-vvv' option, invalid argument: vv\n"
-    //     "Try `foo --help' or `foo --usage' for more information.\n", err);
+    eqint(CARG_OK, carg_parse_string(&carg, "foo -vvv", NULL));
+    eqint(CLOG_DEBUG, clog_verbosity);
 }
 
 
-// static void
-// test_verbose_long() {
-//     struct carg carg = {
-//         .args = NULL,
-//         .header = NULL,
-//         .eat = NULL,
-//         .options = NULL,
-//         .footer = NULL,
-//         .version = "foo 1.2.3",
-//         .flags = 0,
-//     };
-//
-//     clog_verbosity = -1;
-//     eqint(CARG_OK, carg_parse_string(&carg, "foo --verbose", NULL));
-//     eqint(CLOG_INFO, clog_verbosity);
-//
-//     clog_verbosity = -1;
-//     eqint(CARG_OK, carg_parse_string(&carg, "foo --verbose d", NULL));
-//     eqint(CLOG_DEBUG, clog_verbosity);
-//
-//     clog_verbosity = -1;
-//     eqint(CARG_OK, carg_parse_string(&carg, "foo --verbose debug", NULL));
-//     eqint(CLOG_DEBUG, clog_verbosity);
-//
-//     clog_verbosity = -1;
-//     eqint(CARG_OK, carg_parse_string(&carg, "foo --verbose=debug", NULL));
-//     eqint(CLOG_DEBUG, clog_verbosity);
-//
-//     clog_verbosity = -1;
-//     eqint(CARG_OK, carg_parse_string(&carg, "foo --verbose 2", NULL));
-//     eqint(CLOG_ERROR, clog_verbosity);
-// }
+static void
+test_verbose_long() {
+    struct carg carg = {
+        .args = NULL,
+        .header = NULL,
+        .eat = NULL,
+        .options = NULL,
+        .footer = NULL,
+        .version = NULL,
+        .flags = 0,
+    };
+
+    clog_verbosity = -1;
+    eqint(CARG_ERROR, carg_parse_string(&carg, "foo --verbosity", NULL));
+
+    clog_verbosity = -1;
+    eqint(CARG_OK, carg_parse_string(&carg, "foo --verbosity d", NULL));
+    eqint(CLOG_DEBUG, clog_verbosity);
+
+    clog_verbosity = -1;
+    eqint(CARG_OK, carg_parse_string(&carg, "foo --verbosity debug", NULL));
+    eqint(CLOG_DEBUG, clog_verbosity);
+
+    clog_verbosity = -1;
+    eqint(CARG_OK, carg_parse_string(&carg, "foo --verbosity=debug", NULL));
+    eqint(CLOG_DEBUG, clog_verbosity);
+
+    clog_verbosity = -1;
+    eqint(CARG_OK, carg_parse_string(&carg, "foo --verbosity 2", NULL));
+    eqint(CLOG_ERROR, clog_verbosity);
+}
 
 
 int
 main() {
     test_verbose_short();
-    // test_verbose_long();
+    test_verbose_long();
     return EXIT_SUCCESS;
 }
